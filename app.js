035f78e2398f87786fb69d52e918e06c62e8065b
Jezda1337 <megasrbin1@gmail.com>
2021-08-11 14:43:37 +0200

    fix API

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-let search = document.getElementById('search');
-let ip = document.querySelector('#ip');
-let loc = document.getElementById('location');
-let timeZone = document.getElementById('timeZone');
-let isp = document.getElementById('isp');
-const btn = document.getElementById('btn');
+const ip = document.querySelector('#ip');
+const loc = document.querySelector('#location');
+const timeZone = document.querySelector('#timeZone');
+const isp = document.querySelector('#isp');
+const btn = document.querySelector('#btn');
+
 async function request() {
-    const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_e6iOITdkVqVzLXCSCV88u71Pz86TD&ipAddress=${search.value}`);
-    const data = await response.json();
-    ip.innerHTML = data.ip;
-    loc.innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
-    timeZone.innerHTML = data.location.timezone;
-    isp.innerHTML = data.isp;
-    let lat = data.location.lat;
-    let lng = data.location.lng;    
-    getMap(lat,lng);
+    const searchValue = document.querySelector('#search');
+    try {
+        const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_e6iOITdkVqVzLXCSCV88u71Pz86TD&ipAddress=${searchValue.value}`);
+        const data = await response.json();
+        ip.innerHTML = data.ip;
+        loc.innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+        timeZone.innerHTML = data.location.timezone;
+        isp.innerHTML = data.isp;
+        let lat = data.location.lat;
+        let lng = data.location.lng;
+        getMap(lat, lng);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
 }
 
-search.addEventListener('keypress', e =>{
-    if(e.key === 'Enter' && !search.value == '') {
+document.forms[0].addEventListener('submite', e => {
+    e.preventDefault();
+    if (e.key === 'Enter' && !search.value == '') {
         request();
-    }else {
+    } else {
         alert('Enter IP Adress')
     }
 })
-btn.addEventListener('click', ()=>{
-    if(!search.value == '') {
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (!search.value == '') {
         request();
-    }else {
+    } else {
         alert('Enter IP Adress')
     }
 })
@@ -37,12 +49,14 @@ function getMap(lat, lng) {
     let map = L.map('map', {
         center: [lat, lng],
         zoom: 13
-      });
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    });
+    L.tileLayer(`http://{s}.tile.osm.org/{z}/{x}/{y}.png`, {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-      let icon = L.icon({
-          iconUrl: './images/icon-location.svg'
-      })
-      L.marker([lat, lng], {icon: icon}).addTo(map);
+    }).addTo(map);
+    let icon = L.icon({
+        iconUrl: './images/icon-location.svg'
+    })
+    L.marker([lat, lng], { icon: icon }).addTo(map);
 }
+
+
